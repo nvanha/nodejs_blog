76b5d3f9902e3cd1769f54190ddf26fa0fda2698
nvanha <nvha1120@gmail.com>
2021-11-15 10:06:06 +0700

    add mongoose

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
-const port = 3000;
+const port = 4000;
 
-const route = require('./routes/index');
+const route = require("./routes/index");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Middleware
 app.use(
   express.urlencoded({
     extended: true,
-  }),
+  })
 );
 app.use(express.json());
 
@@ -22,17 +22,17 @@ app.use(express.json());
  */
 
 // HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Template engine
 app.engine(
-  'hbs',
+  "hbs",
   handlebars({
-    extname: '.hbs',
-  }),
+    extname: ".hbs",
+  })
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 // Route init
 route(app);
